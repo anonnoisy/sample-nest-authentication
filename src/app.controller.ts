769d1e1e4c2f65883e23a6c0ec571ae54526372d
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-01-28 15:23:00 +0700

    use jwt auth to get access token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly appService: AppService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Req() request: Request) {
-    return request.user;
+    return this.authService.login(request.user);
   }
 
   @Get()
