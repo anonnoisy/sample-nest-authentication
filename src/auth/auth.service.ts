bf39aee6568ed89c3e3124aa972fa72d85981403
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-01-28 15:04:58 +0700

    add auth strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { UsersService } from 'src/users/users.service';
 export class AuthService {
     constructor(private readonly userService: UsersService) {}
 
-    async valdiateUser(username: string, password: string): Promise<any> {
+    async validateUser(username: string, password: string): Promise<any> {
         const user = await this.userService.findOne(username);
         if (user.password === password) {
             const {password, ...result} = user;
